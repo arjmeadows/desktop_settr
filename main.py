714c81384174fc2cd77ac15595c9251a3f9fda96
adammeadows <arjmfreelance@gmail.com>
2025-12-25 16:25:08 +0000

    Implement profile creation with user input and store profiles in the database

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import subprocess
 import pathlib
 import profiles
+import navigation
 
 # Create simple, menu, ability to store profiles, profiles store a list of commands that run when that profile is ran. Profiles stored in database.
 
+navigation.print_logo()
 profiles.Profile.create_profile()
--- profiles.py
+++ profiles.py
@@ -2,10 +2,6 @@ import os
 import sqlite3
 import database
 
-# profile contains a series of apps - which all contain: a title, a description, up to 6 app names, each with their own filename (probs a tuple?) object needs a list one app, obtain can contain the necesary tuples
-
-# a profile is an object. contains list of tuples, cycle through list to launch profile contents
-
 class Profile:
     def __init__(self, name: str, description: str, app_list: list):
         self.name = name
@@ -16,30 +12,30 @@ class Profile:
     def __str__(self):
         return f"{self.name}, {self.description}, {self.app_list}"
 
+    @classmethod
+    def create_profile(self):
+        self.name = input("What do you want to call the profile?: ")
+        self.description = input("Give the profile a description: ")
+        self.temp_app_list = []
+        
+        max_apps_per_profile = 1
+        tracker = 1
+  
+        while tracker <= max_apps_per_profile:
 
-def create_profile(self):
-    name = input("What do you want to call the profile?: ")
-    description = input("Give the profile a description: ")
-    max_apps_per_profile = 6
-    tracker = 0
-    temp_app_list = []
+            app_name = input(f"App name{tracker}: ")
+            app_file_path = input(f"Filepath{tracker}: ")
+            app_tuple = (app_name, app_file_path)
+            self.temp_app_list.append(app_tuple)
+            tracker += 1
 
-    while tracker <= max_apps_per_profile:
+        new_profile = Profile(self.name, self.description, self.temp_app_list)
+        print(new_profile)
+        database.store_profile(new_profile)
 
-        app_name = input("")
-        app_file_path = input("Filepath: ")
-        app_tuple = (app_name, app_file_path)
-        temp_app_list.append(app_tuple)
-
-    new_profile = Profile(name, description, temp_app_list)
-    
-    
-    # database.store_profile(new_profile)
-
-
-    def run_profile(profile: Profile):
-        # needs a loop that iterates through - object probably needs to store number of apps it adds somewhere, use list len.
-        os.subprocess.run(["open", profile.]) #this needs to look in the dictionary/object for the corresponding value. objects seem cleaner?
+# def run_profile(profile: Profile):
+    # needs a loop that iterates through - object probably needs to store number of apps it adds somewhere, use list len.
+    # os.subprocess.run(["open", profile.]) #this needs to look in the dictionary/object for the corresponding value. objects seem cleaner?
         
 
 
