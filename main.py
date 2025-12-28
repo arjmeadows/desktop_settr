131d3974baad7aeadb7fb214fa3b0d12bef098bb
adammeadows <arjmfreelance@gmail.com>
2025-12-28 16:57:01 +0000

    Refactor main.py: enable logo printing and streamline main menu execution

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ import navigation
 # create database
 database.create_profile_list_db()
 database.create_app_list_db()
-# navigation.print_logo()
+navigation.print_logo()
+navigation.main_menu()
+
 # profiles.Profile.create_profile()
 
 # need to look to ask them about/create profile
